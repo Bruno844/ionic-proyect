512b8c2d362ff2a96cca5b44418b561406d4305e
Bruno <brunoruiz050@gmail.com>
2021-06-29 14:08:34 -0300

    mod-autor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
   },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./paginas/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'autor',
+    loadChildren: () => import('./paginas/autor/autor.module').then( m => m.AutorPageModule)
+  },
+  {
+    path: 'lista-libros',
+    loadChildren: () => import('./paginas/lista-libros/lista-libros.module').then( m => m.ListaLibrosPageModule)
+  },
+  {
+    path: 'curriculum-autor',
+    loadChildren: () => import('./paginas/curriculum-autor/curriculum-autor.module').then( m => m.CurriculumAutorPageModule)
+  }
 ];
 
 @NgModule({
--- src/app/paginas/lista-libros/lista-libros.page.spec.ts
+++ src/app/paginas/lista-libros/lista-libros.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { ListaLibrosPage } from './lista-libros.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('ListaLibrosPage', () => {
+  let component: ListaLibrosPage;
+  let fixture: ComponentFixture<ListaLibrosPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ ListaLibrosPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(ListaLibrosPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
